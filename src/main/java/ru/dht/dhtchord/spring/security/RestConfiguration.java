fed916388b4bac333cca28a4af08beb50ba4f020
e2xen <max.ks1230@gmail.com>
2023-12-15 23:24:46 +0300

    add timeout to client

--- src/main/java/ru/dht/dhtchord/spring/security/RestConfiguration.java
+++ src/main/java/ru/dht/dhtchord/spring/security/RestConfiguration.java
@@ -1,10 +1,14 @@
 package ru.dht.dhtchord.spring.security;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @AllArgsConstructor
 @Configuration
@@ -12,9 +16,14 @@ public class RestConfiguration {
 
     private final NodeCredentialsConfig nodeCredentialsConfig;
 
+    private static final Duration TIMEOUT = Duration.ofMillis(5000);
+
     @Bean
     RestTemplate rest() {
-        RestTemplate restTemplate = new RestTemplate();
+        RestTemplate restTemplate = new RestTemplateBuilder()
+                .setReadTimeout(TIMEOUT)
+                .setConnectTimeout(TIMEOUT)
+                .build();
         restTemplate.getInterceptors().add(
                 new BasicAuthenticationInterceptor(nodeCredentialsConfig.getUsername(), nodeCredentialsConfig.getPassword())
         );
